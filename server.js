7646fa668eeb4b89b9b15e76ecfe12389eaf2851
ryaboymb-cpu <ryaboymb@gmail.com>
2026-03-05 16:20:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,51 @@
 const express = require("express")
 const http = require("http")
-const {Server} = require("socket.io")
+const { Server } = require("socket.io")
 const mongoose = require("mongoose")
 require("dotenv").config()
 
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server)
 
-app.use(express.static("public"))
+const io = new Server(server,{
+cors:{origin:"*"}
+})
+
 app.use(express.json())
+app.use(express.static("public"))
+
+/*
+MONGODB
+*/
 
 mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("MongoDB connected"))
+.catch(err=>console.log("Mongo error:",err))
+
+/*
+USER MODEL
+*/
+
+const User = mongoose.model("User",{
+
+telegramId:String,
+username:String,
+
+balanceReal:{
+type:Number,
+default:0
+},
+
+balanceDemo:{
+type:Number,
+default:1000
+}
+
+})
+
+/*
+ONLINE SYSTEM
+*/
 
 let online = 0
 
@@ -31,8 +65,180 @@ io.emit("online",online)
 
 })
 
-server.listen(3000,()=>{
+/*
+AUTH TELEGRAM
+*/
 
-console.log("Server started")
+app.post("/api/auth",async(req,res)=>{
+
+const {telegramId,username} = req.body
+
+let user = await User.findOne({telegramId})
+
+if(!user){
+
+user = await User.create({
+
+telegramId,
+username
+
+})
+
+}
+
+res.json(user)
+
+})
+
+/*
+GET BALANCE
+*/
+
+app.get("/api/balance/:id",async(req,res)=>{
+
+const user = await User.findOne({telegramId:req.params.id})
+
+res.json({
+
+real:user.balanceReal,
+demo:user.balanceDemo
+
+})
+
+})
+
+/*
+CRASH GAME
+*/
+
+function generateCrash(){
+
+let r = Math.random()
+
+let crash = (1/(1-r))
+
+return Math.min(crash,10)
+
+}
+
+app.get("/api/crash",async(req,res)=>{
+
+const multiplier = generateCrash()
+
+res.json({multiplier})
+
+})
+
+/*
+MINES GAME
+*/
+
+app.post("/api/mines",async(req,res)=>{
+
+const {bet} = req.body
+
+let mine = Math.random()<0.2
+
+if(mine){
+
+res.json({result:"lose"})
+
+}else{
+
+res.json({
+
+result:"win",
+
+multiplier:1.3
+
+})
+
+}
+
+})
+
+/*
+BET SYSTEM
+*/
+
+app.post("/api/bet",async(req,res)=>{
+
+const {telegramId,amount,type} = req.body
+
+const user = await User.findOne({telegramId})
+
+if(type==="demo"){
+
+if(user.balanceDemo<amount){
+
+return res.json({error:"no balance"})
+
+}
+
+user.balanceDemo -= amount
+
+}else{
+
+if(user.balanceReal<amount){
+
+return res.json({error:"no balance"})
+
+}
+
+user.balanceReal -= amount
+
+}
+
+await user.save()
+
+res.json({
+
+demo:user.balanceDemo,
+real:user.balanceReal
+
+})
+
+})
+
+/*
+WIN SYSTEM
+*/
+
+app.post("/api/win",async(req,res)=>{
+
+const {telegramId,amount,type} = req.body
+
+const user = await User.findOne({telegramId})
+
+if(type==="demo"){
+
+user.balanceDemo += amount
+
+}else{
+
+user.balanceReal += amount
+
+}
+
+await user.save()
+
+res.json({
+
+demo:user.balanceDemo,
+real:user.balanceReal
+
+})
+
+})
+
+/*
+START SERVER
+*/
+
+const PORT = process.env.PORT || 3000
+
+server.listen(PORT,()=>{
+
+console.log("Loonx Gifts server running")
 
 })
